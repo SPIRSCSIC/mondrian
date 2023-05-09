896063c9bfe5644b1d56ba3ea5d3a74325ed618e
Sergio Chica <sergio.chica@uc3m.es>
2023-05-09 13:32:59 +0200

    Changed a bit the function definitions. Used clang-format.

--- src/mondrian.c
+++ src/mondrian.c
@@ -1,43 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include <limits.h>
 #include <math.h>
-#include <unistd.h>
-#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int DEFAULT_REC = 10;
 int DEFAULT_ATT = 3;
 int DEFAULT_K = 2;
-char* DEFAULT_MAP = "map.json";
+char *DEFAULT_MAP = "map.json";
 
+struct config {
+  int n_records;
+  int n_attributes;
+  int k;
+  char *file_path;
+};
 
 int compare(const void *a, const void *b) {
-  int f_a = *((int*)a);
-  int f_b = *((int*)b);
+  int f_a = *((int *)a);
+  int f_b = *((int *)b);
 
   return f_a - f_b;
 }
 
-void anonymized_partition(int **data, int size, int num_attributes) {
-  int* min_vals = malloc(sizeof(int*) * num_attributes);
-  int* max_vals = malloc(sizeof(int*) * num_attributes);
-  for(int i = 0; i < num_attributes; i++) {
+void anonymized_partition(struct config cfg, int **data) {
+  int *min_vals = malloc(sizeof(int *) * cfg.n_attributes);
+  int *max_vals = malloc(sizeof(int *) * cfg.n_attributes);
+  for (int i = 0; i < cfg.n_attributes; i++) {
     min_vals[i] = INT_MAX;
     max_vals[i] = INT_MIN;
   }
-  for(int i = 0; i < num_attributes; i++){
-    for(int j = 0; j < size; j++) {
-      if (data[j][i] < min_vals[i]) min_vals[i] = data[j][i];
-      if (data[j][i] > max_vals[i]) max_vals[i] = data[j][i];
+  for (int i = 0; i < cfg.n_attributes; i++) {
+    for (int j = 0; j < cfg.n_records; j++) {
+      if (data[j][i] < min_vals[i])
+        min_vals[i] = data[j][i];
+      if (data[j][i] > max_vals[i])
+        max_vals[i] = data[j][i];
     }
   }
-  for(int i = 0; i < size; i++) {
-    for(int j = 0; j < num_attributes; j++) {
-      if(min_vals[j] == max_vals[j]){
+  for (int i = 0; i < cfg.n_records; i++) {
+    for (int j = 0; j < cfg.n_attributes; j++) {
+      if (min_vals[j] == max_vals[j]) {
         printf("%d ", data[i][j]);
-      }
-      else {
+      } else {
         printf("[%d, %d] ", min_vals[j], max_vals[j]);
       }
     }
@@ -45,15 +52,16 @@ void anonymized_partition(int **data, int size, int num_attributes) {
   }
 }
 
-void find_largest_range(int **data, int size, int num_attributes, int *attribute_idx, int *range) {
+void find_largest_range(struct config cfg, int **data, int *attribute_idx,
+                        int *range) {
   int max_range = INT_MIN;
   *attribute_idx = -1;
 
-  for (int attr = 0; attr < num_attributes; attr++) {
+  for (int attr = 0; attr < cfg.n_attributes; attr++) {
     int min = INT_MAX;
     int max = INT_MIN;
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < cfg.n_records; i++) {
       if (data[i][attr] < min)
         min = data[i][attr];
       if (data[i][attr] > max)
@@ -70,15 +78,15 @@ void find_largest_range(int **data, int size, int num_attributes, int *attribute
   *range = max_range;
 }
 
-void mondrian(int **data, int num_attributes, int k, int size) {
-  if (size < 2*k) {
-    anonymized_partition(data, size, num_attributes);
+void mondrian(struct config cfg, int **data) {
+  if (cfg.n_records < 2 * cfg.k) {
+    anonymized_partition(cfg, data);
     return;
   }
 
   int attribute_idx;
   int range;
-  find_largest_range(data, size, num_attributes, &attribute_idx, &range);
+  find_largest_range(cfg, data, &attribute_idx, &range);
 
   if (attribute_idx == -1) {
     return;
@@ -89,124 +97,138 @@ void mondrian(int **data, int num_attributes, int k, int size) {
   int l_size;
   int r_size = 0;
   int median;
-  while(!allowable_cut) {
-    int *values = (int *)malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
-      values[i] = data[i][(attribute_idx + iterator) % num_attributes];
+  while (!allowable_cut) {
+    int *values = (int *)malloc(cfg.n_records * sizeof(int));
+    for (int i = 0; i < cfg.n_records; i++) {
+      values[i] = data[i][(attribute_idx + iterator) % cfg.n_attributes];
     }
 
-    qsort(values, size, sizeof(int), compare);
-    median = values[size / 2];
-    // printf("Cut by: %d, %d\n", (attribute_idx + iterator) % num_attributes, median);
+    qsort(values, cfg.n_records, sizeof(int), compare);
+    median = values[cfg.n_records / 2];
+    // printf("Cut by: %d, %d\n", (attribute_idx + iterator) % cfg.n_attributes,
+    // median);
     free(values);
 
     r_size = 0;
 
-    for (int i = 0; i < size; i++) {
-      if(data[i][(attribute_idx + iterator) % num_attributes] >= median) {
+    for (int i = 0; i < cfg.n_records; i++) {
+      if (data[i][(attribute_idx + iterator) % cfg.n_attributes] >= median) {
         r_size += 1;
       }
     }
-    l_size = size - r_size;
-    if(r_size == size || l_size == size || r_size < k || l_size < k) {
+    l_size = cfg.n_records - r_size;
+    if (r_size == cfg.n_records || l_size == cfg.n_records || r_size < cfg.k ||
+        l_size < cfg.k) {
       iterator += 1;
-      if(iterator == num_attributes) {
-        anonymized_partition(data, size, num_attributes);
+      if (iterator == cfg.n_attributes) {
+        anonymized_partition(cfg, data);
         return;
       }
       continue;
     }
     allowable_cut = 1;
-    attribute_idx = (attribute_idx + iterator) % num_attributes;
+    attribute_idx = (attribute_idx + iterator) % cfg.n_attributes;
   }
-  int** r_part = (int**)malloc(sizeof(int*)*r_size);
-  int** l_part = (int**)malloc(sizeof(int*)*l_size);
+  int **r_part = (int **)malloc(sizeof(int *) * r_size);
+  int **l_part = (int **)malloc(sizeof(int *) * l_size);
   int r_count = 0;
   int l_count = 0;
-  for (int i = 0; i < size; i++) {
-    if(data[i][attribute_idx] >= median) {
+  for (int i = 0; i < cfg.n_records; i++) {
+    if (data[i][attribute_idx] >= median) {
       r_part[r_count] = data[i];
       r_count++;
-    }
-    else {
+    } else {
       l_part[l_count] = data[i];
       l_count++;
     }
   }
 
-  mondrian(r_part, num_attributes, k, r_size);
-  mondrian(l_part, num_attributes, k, l_size);
+  struct config r_cfg = {r_size, cfg.n_attributes, cfg.k, cfg.file_path};
+  struct config l_cfg = {l_size, cfg.n_attributes, cfg.k, cfg.file_path};
+
+  mondrian(r_cfg, r_part);
+  mondrian(l_cfg, l_part);
 }
 
-struct Arguments
-{
-  int num_records;
-  int num_attributes;
-  int k;
-  char *file_path;
-};
-
-struct Arguments parse_args(int argc, char **argv)
-{
-  struct Arguments args = {DEFAULT_REC, DEFAULT_ATT, DEFAULT_K, DEFAULT_MAP};
+struct config parse_args(int argc, char **argv) {
+  struct config args = {DEFAULT_REC, DEFAULT_ATT, DEFAULT_K, DEFAULT_MAP};
   int c;
-  while ((c = getopt (argc, argv, ":k:r:a:f:")) != -1)
-    {
-      switch (c)
-        {
-        case 'k': // k_range
-          args.k = atoi(optarg);
-          break;
-        case 'r': // n_records
-          args.num_records = atoi(optarg);
-          break;
-        case 'a': // n_attributes
-          args.num_attributes = atoi(optarg);
-          break;
-        case 'f': // file
-          args.file_path = optarg;
-          break;
-        case '?':
-          fprintf (stderr,
-                   "Usage: %s [OPTION...]\n"
-                   "  -k <value>\t\tSet the K-anonimity\n"
-                   "  -r <value>\t\tSet the number of records\n"
-                   "  -a <value>\t\tSet number of attributes\n"
-                   "  -f <value>\t\tSet the file path to anonimyze\n"
-                   , *argv);
-          exit(1);
-        case ':':
-          fprintf (stderr, "Error: option -%c requires an argument\n", optopt);
-          exit(1);
-        }
+  while ((c = getopt(argc, argv, ":k:r:a:f:")) != -1) {
+    switch (c) {
+    case 'k': // k_range
+      args.k = atoi(optarg);
+      break;
+    case 'r': // n_records
+      args.n_records = atoi(optarg);
+      break;
+    case 'a': // n_attributes
+      args.n_attributes = atoi(optarg);
+      break;
+    case 'f': // file
+      args.file_path = optarg;
+      break;
+    case '?':
+      fprintf(stderr,
+              "Usage: %s [OPTION...]\n"
+              "  -k <value>\t\tSet the K-anonimity\n"
+              "  -r <value>\t\tSet the number of records\n"
+              "  -a <value>\t\tSet number of attributes\n"
+              "  -f <value>\t\tSet the file path to anonimyze\n",
+              *argv);
+      exit(1);
+    case ':':
+      fprintf(stderr, "Error: option -%c requires an argument\n", optopt);
+      exit(1);
     }
+  }
   return args;
 }
 
 int main(int argc, char **argv) {
-  struct Arguments args = parse_args(argc, argv);
+  struct config cfg = parse_args(argc, argv);
 
-  int **data = (int**)malloc(args.num_records * sizeof(int*));
-  for (int i = 0; i < args.num_records; i++) {
-    data[i] = (int*)malloc(args.num_attributes * sizeof(int));
+  int **data = (int **)malloc(cfg.n_records * sizeof(int *));
+  for (int i = 0; i < cfg.n_records; i++) {
+    data[i] = (int *)malloc(cfg.n_attributes * sizeof(int));
   }
 
   // Example dataset
-  data[0][0] = 34; data[0][1] = 12; data[0][2] = 1;
-  data[1][0] = 20; data[1][1] = 9;  data[1][2] = 5;
-  data[2][0] = 25; data[2][1] = 7;  data[2][2] = 3;
-  data[3][0] = 50; data[3][1] = 15; data[3][2] = 6;
-  data[4][0] = 55; data[4][1] = 20; data[4][2] = 7;
-  data[5][0] = 40; data[5][1] = 12; data[5][2] = 2;
-  data[6][0] = 30; data[6][1] = 10; data[6][2] = 4;
-  data[7][0] = 45; data[7][1] = 18; data[7][2] = 6;
-  data[8][0] = 60; data[8][1] = 22; data[8][2] = 8;
-  data[9][0] = 15; data[9][1] = 5;  data[9][2] = 3;
+  data[0][0] = 34;
+  data[0][1] = 12;
+  data[0][2] = 1;
+  data[1][0] = 20;
+  data[1][1] = 9;
+  data[1][2] = 5;
+  data[2][0] = 25;
+  data[2][1] = 7;
+  data[2][2] = 3;
+  data[3][0] = 50;
+  data[3][1] = 15;
+  data[3][2] = 6;
+  data[4][0] = 55;
+  data[4][1] = 20;
+  data[4][2] = 7;
+  data[5][0] = 40;
+  data[5][1] = 12;
+  data[5][2] = 2;
+  data[6][0] = 30;
+  data[6][1] = 10;
+  data[6][2] = 4;
+  data[7][0] = 45;
+  data[7][1] = 18;
+  data[7][2] = 6;
+  data[8][0] = 60;
+  data[8][1] = 22;
+  data[8][2] = 8;
+  data[9][0] = 15;
+  data[9][1] = 5;
+  data[9][2] = 3;
 
-  mondrian(data, args.num_attributes, args.k, args.num_records);
+  // cfg.n_attributes, cfg.k, cfg.n_records
+  mondrian(cfg, data);
 
   // Free the memory allocated for the data array
-  for (int i = 0; i < args.num_records; i++) {
+  for (int i = 0; i < cfg.n_records; i++) {
     free(data[i]);
   }
   free(data);
